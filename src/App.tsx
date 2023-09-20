fa4ef5c31a40a38a90ac2fcc4835d594b10a5347
yungcypo <cypooriginal@gmail.com>
2023-09-20 22:54:48 +0200

    started with landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { PageNotFound } from "./pages/PageNotFound";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />}/>
+                    <Route />
+                    <Route path="*" element={<PageNotFound />}/>
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
